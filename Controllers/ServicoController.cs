ff19cf7eec96413cb35a4866b84900ac1726d71b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-01 10:09:15 -0300

    Replaced generic API controller with controller to import file records and export record statistics

--- Controllers/ServicoController.cs
+++ Controllers/ServicoController.cs
@@ -15,124 +15,45 @@ namespace backend.Controllers
     public class ServicoController : ControllerBase
     {
         private readonly Database _context;
-
         public ServicoController(Database context)
         {
             _context = context;
         }
-
-        // GET: api/Servico
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Servico>>> Getrelatorio()
-        {
-          if (_context.relatorio == null)
-          {
-              return NotFound();
-          }
-            return await _context.relatorio.ToListAsync();
-        }
-
-        // GET: api/Servico/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Servico>> GetServico(int id)
-        {
-          if (_context.relatorio == null)
-          {
-              return NotFound();
-          }
-            var servico = await _context.relatorio.FindAsync(id);
-
-            if (servico == null)
-            {
-                return NotFound();
-            }
-
-            return servico;
-        }
-
-        // PUT: api/Servico/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutServico(int id, Servico servico)
-        {
-            if (id != servico.indentificador)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(servico).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ServicoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Servico
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Servico>> PostServico(Servico servico)
+        public ActionResult PostServico(IFormFile file)
         {
-          if (_context.relatorio == null)
-          {
-              return Problem("Entity set 'Database.relatorio'  is null.");
-          }
-            _context.relatorio.Add(servico);
+            if (file.Length == 0) return BadRequest("O arquivo enviado está vazio!");
+            var filemanager = new FileManager(_context, file);
+            var relatorio = filemanager.Relatorio();
             try
             {
-                await _context.SaveChangesAsync();
+              _context.AddRange(relatorio);
+              _context.SaveChanges();
+              return CreatedAtAction("GetServico", null, relatorio);
             }
-            catch (DbUpdateException)
+            catch (System.InvalidOperationException erro)
             {
-                if (ServicoExists(servico.indentificador))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
+              return BadRequest(erro.Message);
             }
-
-            return CreatedAtAction("GetServico", new { id = servico.indentificador }, servico);
+            catch (Microsoft.EntityFrameworkCore.DbUpdateException erro)
+            {
+              return BadRequest(erro.InnerException?.Message);
+            }
+            catch (System.Exception erro)
+            {
+              return Problem(erro.Message);
+            }
         }
-
-        // DELETE: api/Servico/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteServico(int id)
+        [HttpGet]
+        public ActionResult GetServico()
         {
-            if (_context.relatorio == null)
-            {
-                return NotFound();
-            }
-            var servico = await _context.relatorio.FindAsync(id);
-            if (servico == null)
-            {
-                return NotFound();
-            }
-
-            _context.relatorio.Remove(servico);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+          var relatorios = from f in _context.relatorio group f by f.filename into g select new {g.Key, Count = g.Count() };
+          return Ok(relatorios);
         }
-
-        private bool ServicoExists(int id)
+        [HttpDelete("{filename}")]
+        public ActionResult DeleteServico(string filename)
         {
-            return (_context.relatorio?.Any(e => e.indentificador == id)).GetValueOrDefault();
+          return NoContent();
         }
     }
 }
