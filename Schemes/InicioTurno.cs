d72b5feb29c4cacc7da46aaee03afbd16ea3f330
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-01 09:53:18 -0300

    Removed class to map records that are not work orders

--- Schemes/InicioTurno.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Models;
-public class InicioTurno
-{
-  public string? placa { get; set; }
-  public int id_motorista { get; set; }
-  public int id_ajudante { get; set; }
-  public int id_tecnico { get; set; }
-}
