ecefe2b9333152c33300163846fad3a23b000fd7
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-01 09:59:00 -0300

    1. Added attributes to map non-work order records 2. Added attribute invalidity due to lack of attributes of records that are not services; 3. Removed validable class inheritance, as it will be done by the object's handling class;

--- Schemes/Servicos.cs
+++ Schemes/Servicos.cs
@@ -2,29 +2,43 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models;
-public class Servico : IEntity, IValidatableObject
+public class Servico
 {
-  public string recurso { get; set; }
+  public String filename { get; set; }
+  public String recurso { get; set; }
   public DateOnly dia { get; set; }
   [Key]
   [DatabaseGenerated(DatabaseGeneratedOption.None)]
-  public int indentificador { get; set; }
-  public string status { get; set; }
+  public Int64 indentificador { get; set; }
+  public TipoStatus status { get; set; }
+  public String? nome_do_cliente { get; set; }
+  public String? endereco_destino { get; set; }
+  public String? cidade_destino { get; set; }
+  public String? codigo_postal { get; set; }
   public TimeOnly? hora_inicio { get; set; }
   public TimeOnly? hora_final { get; set; }
-  public TimeOnly? duracao_feito { get; set; }
-  public TimeOnly? desloca_feito { get; set; }
-  public string? tipo_atividade { get; set; }
-  public long servico { get; set; }
-  public string? codigos { get; set; }
-  public string? observacao { get; set; }
-  public int? instalacao { get; set; }
-  public string tipo_servico { get; set; }
-  public TimeOnly? Desloca_estima { get; set; }
-  public TimeOnly? duracao_estima { get; set; }
-
-  public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-  {
-    throw new NotImplementedException();
-  }
+  public DateTime? vencimento { get; set; }
+  public TimeSpan? duracao_feito { get; set; }
+  public TimeSpan? desloca_feito { get; set; }
+  public String? tipo_atividade { get; set; }
+  public Int64? servico { get; set; }
+  public String? area_trabalho { get; set; }
+  public String? codigos { get; set; }
+  public String? id_viatura { get; set; }
+  public Int32? id_motorista { get; set; }
+  public Int32? id_ajudante { get; set; }
+  public Int32? id_tecnico { get; set; }
+  public String? observacao { get; set; }
+  public String? bairro_destino { get; set; }
+  public Int32? instalacao { get; set; }
+  public String? complemento_destino { get; set; }
+  public String? referencia_destino { get; set; }
+  public String? tipo_servico { get; set; }
+  public Double? debitos_cliente { get; set; }
+  public TipoInstalacao? tipo_instalacao { get; set; }
+  public TimeSpan? Desloca_estima { get; set; }
+  public TimeSpan? duracao_estima { get; set; }
+  public String? motivo_indisponibilidade { get; set; }
 }
+public enum TipoInstalacao { Monofasico, Bifasico, Trifasico }
+public enum TipoStatus {cancelado, concluido, deslocando, iniciado, rejeitado, pendente}
