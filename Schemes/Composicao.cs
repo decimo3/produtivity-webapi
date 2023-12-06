a5e60deec70b61a6dc712b334fc4d08df5da396c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-06 13:49:21 -0300

    Removed redundant validation

--- Schemes/Composicao.cs
+++ Schemes/Composicao.cs
@@ -6,29 +6,24 @@ public class Composicao
   [Required]
   public DateOnly dia { get; set; }
   [Required]
-  [Range(11400, 17000, ErrorMessage = "O valor do adesivo não é válido!")]
   public int adesivo { get; set; }
   [Required]
   public string? placa { get; set; }
   [Required]
-  [StringLength(32, ErrorMessage = "O nome do recurso é muito longo!")]
   public string recurso { get; set; }
   [Required]
   public Atividade atividade { get; set; }
   [Required]
   public string? motorista { get; set; }
   [Required]
-  [RegularExpression("^[0-9]{7}$")]
   public int id_motorista { get; set; }
   [Required]
   public string? ajudante { get; set; }
   [Required]
-  [RegularExpression("^[0-9]{7}$")]
   public int id_ajudante { get; set; }
   [Required]
   public Int64 telefone { get; set; }
   [Required]
-  [RegularExpression("^[0-9]{7}$")]
   public int id_supervisor { get; set; }
   [Required]
   public string? supervisor { get; set; }
