137db10d831964a73dcc9b327f82c950c04788b9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-08 10:32:11 -0300

    1. controller to process authorization requests 2. attach cookie if authentication is successful

---
+++ Controllers/AutenticacaoController.cs
@@ -0,0 +1,32 @@
+namespace backend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using backend.Services;
+using backend.Models;
+[ApiController]
+[Route("[controller]")]
+public class AutencicacaoController : ControllerBase
+{
+  private readonly AutenticacaoServico autenticacao;
+  private readonly IHttpContextAccessor context;
+  public AutencicacaoController(AutenticacaoServico autenticacao, IHttpContextAccessor context)
+  {
+    this.autenticacao = autenticacao;
+    this.context = context;
+  }
+  [HttpPost]
+  public ActionResult Post(AutenticacaoRequisicao requisicao)
+  {
+    var auth = autenticacao.Authenticate(requisicao);
+    if(auth is null) return Unauthorized();
+    var options = new CookieOptions
+    {
+      Expires = DateTime.Now.AddDays(7),
+      Secure = false, // define a cookie como seguro, somente será enviado em conexões HTTPS.
+      HttpOnly = false, // define a cookie como acessível somente por HTTP, não pode ser acessado por JavaScript.
+      Path = "/",
+      Domain = "localhost"
+    };
+    context.HttpContext.Response.Cookies.Append("MeuCookie", auth.token, options);
+    return Ok(auth);
+  }
+}
