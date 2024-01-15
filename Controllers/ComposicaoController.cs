b646e598518e5220c1a45455f4e203e98fb0d532
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-01-15 13:34:57 -0300

    1. Changed name for the database property; 2. Changed asynchronous methods to synchronous; > There was a concurrency problem due to the use of the database after an asynchronous method without using await; 3. Registration of changes immediately after recording in the database; 4. Refactored try ... catch block to return the error in the response body; 5. Refactored opening code block to just one instruction;

--- Controllers/ComposicaoController.cs
+++ Controllers/ComposicaoController.cs
@@ -29,34 +29,35 @@ namespace backend.Controllers
         [HttpGet("{inicio}/{final}/{regional}/{atividade}")]
         public async Task<ActionResult<IEnumerable<Composicao>>> Getcomposicao(DateOnly inicio, DateOnly final, Regional regional, Atividade atividade)
         {
-          if (_context.composicao == null) return NotFound();
-          if (atividade == Atividade.NENHUM && regional == Regional.NENHUM) return await (from f in _context.composicao where (f.dia >= inicio) && (f.dia <= final) select f).ToListAsync();
-          if (regional == Regional.NENHUM && atividade != Atividade.NENHUM) return await (from f in _context.composicao where (f.dia >= inicio) && (f.dia <= final) && (f.atividade == atividade) select f).ToListAsync();
-          if (atividade == Atividade.NENHUM && regional != Regional.NENHUM) return await (from f in _context.composicao where (f.dia >= inicio) && (f.dia <= final) && (f.regional == regional) select f).ToListAsync();
-          return await (from f in _context.composicao where (f.dia >= inicio) && (f.dia <= final) && (f.regional == regional) && (f.atividade == atividade) select f).ToListAsync();
+          if (database.composicao == null) return NotFound();
+          if (atividade == Atividade.NENHUM && regional == Regional.NENHUM) return await (from f in database.composicao where (f.dia >= inicio) && (f.dia <= final) select f).ToListAsync();
+          if (regional == Regional.NENHUM && atividade != Atividade.NENHUM) return await (from f in database.composicao where (f.dia >= inicio) && (f.dia <= final) && (f.atividade == atividade) select f).ToListAsync();
+          if (atividade == Atividade.NENHUM && regional != Regional.NENHUM) return await (from f in database.composicao where (f.dia >= inicio) && (f.dia <= final) && (f.regional == regional) select f).ToListAsync();
+          return await (from f in database.composicao where (f.dia >= inicio) && (f.dia <= final) && (f.regional == regional) && (f.atividade == atividade) select f).ToListAsync();
         }
         // PUT: api/Composicao/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{data}/{recurso}")]
-        public async Task<IActionResult> PutComposicao(DateOnly data, string recurso, Composicao composicao)
+        public ActionResult PutComposicao(DateOnly data, string recurso, Composicao composicao)
         {
             if (!ComposicaoExists(data, recurso))
             {
                 return NotFound();
             }
+            var c = database.composicao.Find(data, recurso);
+            if (c == null) return NotFound();
             if (recurso != composicao.recurso)
             {
                 if (ComposicaoExists(composicao.dia, composicao.recurso))
                 {
                     return Conflict();
                 }
-                var c = await _context.composicao.FindAsync(data, recurso);
-                if (c == null) return NotFound();
-                _context.composicao.Remove(c);
-                _context.composicao.Add(composicao);
+                database.composicao.Remove(c);
+                database.composicao.Add(composicao);
                 try
                 {
-                  await _context.SaveChangesAsync();
+                  database.SaveChanges();
+                  alteracaoLog.Registrar("PUT", c, composicao);
                   return NoContent();
                 }
                 catch (DbUpdateConcurrencyException erro)
@@ -64,23 +65,17 @@ namespace backend.Controllers
                   return Problem(erro.InnerException?.Message);
                 }
             }
-            _context.Entry(composicao).State = EntityState.Modified;
+            database.Entry(composicao).State = EntityState.Modified;
             try
             {
-                await _context.SaveChangesAsync();
+                database.SaveChanges();
+                alteracaoLog.Registrar("PUT", c, composicao);
+                return NoContent();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException erro)
             {
-                if (!ComposicaoExists(data, recurso))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return Problem(erro.InnerException?.Message);
             }
-            return NoContent();
         }
         // POST: api/Composicao
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -88,7 +83,7 @@ namespace backend.Controllers
         [ActionName("PostFormulario")]
         public ActionResult PostComposicao(Composicao composicao)
         {
-          if (_context.composicao == null)
+          if (database.composicao == null)
           {
               return Problem("Entity set 'Database.composicao'  is null.");
           }
@@ -96,10 +91,11 @@ namespace backend.Controllers
           {
               return Conflict();
           }
-          _context.composicao.Add(composicao);
+          database.composicao.Add(composicao);
           try
           {
-            _context.SaveChangesAsync();
+            database.SaveChanges();
+            alteracaoLog.Registrar("POST", null, composicao);
             return Created("/Composicao", composicao);
           }
           catch (System.InvalidOperationException erro)
@@ -118,45 +114,36 @@ namespace backend.Controllers
 
         // DELETE: api/Composicao/5
         [HttpDelete("{data}/{recurso}")]
-        public async Task<IActionResult> DeleteComposicao(DateOnly data, string recurso)
+        public ActionResult DeleteComposicao(DateOnly data, string recurso)
         {
-            if (_context.composicao == null)
-            {
-                return NotFound();
-            }
-            var composicao = await _context.composicao.FindAsync(data, recurso);
-            if (composicao == null)
-            {
-                return NotFound();
-            }
-
-            _context.composicao.Remove(composicao);
-            await _context.SaveChangesAsync();
-
+            if (database.composicao == null) return NotFound();
+            var composicao = database.composicao.Find(data, recurso);
+            if (composicao == null) return NotFound();
+            database.composicao.Remove(composicao);
+            database.SaveChanges();
+            alteracaoLog.Registrar("DEL", composicao, null);
             return NoContent();
         }
 
         private bool ComposicaoExists(DateOnly data, string recurso)
         {
-            return (_context.composicao?.Any(e => e.recurso == recurso && e.dia == data)).GetValueOrDefault();
+            return (database.composicao?.Any(e => e.recurso == recurso && e.dia == data)).GetValueOrDefault();
         }
         [HttpPost("Arquivo")]
         [ActionName("PostArquivo")]
-        public IActionResult PostComposicao(IFormFile file)
+        public ActionResult PostComposicao(IFormFile file)
         {
             if (file.Length == 0) return BadRequest("O arquivo enviado está vazio!");
-            var filemanager = new FileManager(_context, file);
+            var filemanager = new FileManager(database, file);
             try
             {
               var composicoes = filemanager.Composicao();
               if (composicoes.GroupBy(a => a.dia).Any(b => b.Count() > 1))
                 throw new InvalidOperationException("A composição enviada contém mais de uma data!\nA composição deve ser enviada um dia por vez.");
-              if (composicoes.Where(c => c.validacao.Any()).Any())
-              {
-                return UnprocessableEntity(composicoes);
-              }
-              _context.AddRange(composicoes);
-              _context.SaveChanges();
+              if (composicoes.Where(c => c.validacao.Any()).Any()) return UnprocessableEntity(composicoes);
+              database.AddRange(composicoes);
+              database.SaveChanges();
+              alteracaoLog.Registrar("POST", null, new {filename = file.FileName, linhas = composicoes.Count});
               return CreatedAtAction("GetComposicao", null, composicoes);
             }
             catch (System.InvalidOperationException erro)
